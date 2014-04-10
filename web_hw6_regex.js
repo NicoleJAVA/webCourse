5f32844e9677d2b47e41cb31dd5335c5c05bdfb0
Nicole Chang <powernicole2@yahoo.com.tw>
2014-04-10 22:41:29 +0800

    hw6 Ver.2
    筆記：
    ● Wrong Code :
    urlReg = /<a\s+href\s*=\s*"\s*(.*?)"\s*>.*?<\/a>.*?/i;
    ● Correct Code :
    urlReg = /<a\s+href\s*=\s*"\s*(.*?)"\s*>.*?<\/a>.*?/i  不要有分號 !
    
    筆記：
    ● Wrong Code :
    urlReg = /<a\s+href\s+=\s+"(.*?)"\s+>.*?<\/a>/
    ● Correct Code :
    urlReg = /<a\s+href\s*=\s*"(.*?)"\s*>.*?<\/a>.*?/;
    或是 urlReg = /<a\s+href\s*=\s*"\s*(.*?)"\s*>.*?<\/a>.*?/i
    
    筆記：
    ● Wrong Code :
    WScript.Echo( /*"\n urlPart of str" + c + " is " + */  urlPart ); /**/
    ● Correct Code :
    WScript.Echo( /*"\n urlPart of str" + c + " is " + */  urlPart[1] );
    ● /* Oh !! So stupid, Nicole finally found out the exec() does not
    return a "string", */
    /*but it returns an "array"  ! Therefore, the address is not urlPrt, but
    it's urlPart[1] */
    
    筆記：
    Wrong Code :
    nUrlReg = /<a\s*href\s*=\s*[^" ]*.*>.*<\/a>/gi;
    ● Correct Code :
    nUrlReg = /<a\s*href\s*=[^"]*>.*<\/a>/gi;
    關鍵是 [^"]*.* 和  [^"]* ，不知道為什麼耶 > < 可能是因為網址雖然有雙引號，但是他可以通過 [^"]*
    的測試，因為只要不含雙引號的字串那前幾個字都算是符合 [^"]* ，而接下來網址的雙引號也可以被當作是單純的 .*
    ，所以也會通過，如此一來，所有有雙引號的都會被視為沒有雙引號了，囧啊。
    
    筆記：
    ● Wrong Code :
    path = str3[j].match( nUrlReg );
    ● Correct Code :
    pathReg = /=[\s]*(.*?)[\s]*>/
    path = str3[j].match( nUrlReg );
    多增加一個 pathReg 來專門抽取已知是 non-url link 的字串裡面的純粹是路徑的部分。

--- web_hw6_regex.js
+++ web_hw6_regex.js
@@ -1,26 +1,24 @@
-
+/**/
 url = "http://mirlab.org/jang/courses/webProgramming/homework/linkExtraction/testPage4linkExtraction2.htm";
-xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-xmlHttp.open("GET", url, false, "");
-xmlHttp.send();				// Send the request
-contents = xmlHttp.responseText;	// Get the contents
-/* ----Teacher's original code :  extract the title
-----re = new RegExp("<title>(.*?)<\/title>", "gi");
-*/
-/*----  Nicole's Test :  
-----Wrong Code : 
-----urlReg = /<a\s+href\s+=\s+"(.*?)"\s+>.*?<\/a>.*?/
-----Correct Code : 
-----urlReg = /<a\s+href\s*=\s*"(.*?)"\s*>.*?<\/a>.*?/;
-----Correct basic urlReg : 
-----urlReg = /<a\s+href\s*=\s*"\s*(.*?)"\s*>.*?<\/a>.*?/i
-----Or, 
-----re = new RegExp("<a href(.*?)>(.*?)<\/a>(.*?)", "gi");
-*/
+/***/
+urlNum = 0; /* number of url links */
+nUrlNum = 0; /* number of non-url links*/
+contents = "";
+addrArray = new Array(); /* it holds the address parts of each link */
+titleArray = new Array();
+nUrlArray = new Array();
 
-/* for use 
-WScript.Echo( contents );
-*/
+function extract_TA_homework(){
+    var xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    googleurl = "https://www.google.com.tw";
+    xmlHttp.open("GET", url, false, "");
+    xmlHttp.send();				// Send the request
+    contents = xmlHttp.responseText;	// Get the contents
+re = new RegExp("<title>(.*?)<\/title>", "gi");;
+index=contents.search(re);
+WScript.Echo("\n\nindex = " + index);
+WScript.Echo("title = " + RegExp.$1);
+}
 
 function delete_xmpAndComment(){
     
@@ -35,57 +33,94 @@ function delete_xmpAndComment(){
 }
  
 function test_urlTag(){
-    urlReg = /<a\s+href\s*=\s*"\s*(.*?)"\s*>.*?<\/a>.*?/gi
+    urlReg = /<a\s+href\s*=\s*"\s*(http.*?)"\s*>.*?<\/a>.*?/gi
+    addrReg = /\"(.*?)\"/  /* just the address part */
+    domainReg = /mirlab\.org/
+    /*問號是最小比對。比對到的字串是在比對成功的情況下最短的字串*/
     str = contents.match( urlReg );
-	WScript.Echo( "\nThere are " + str.length + "matching results" );
+	WScript.Echo( "\n\n\nThere are " + str.length + " matching results" );
+    urlnum = str.length;
     for( c=0; c<str.length; c++ ){
-  
-    	urlPart = urlReg.exec( str[c] );
-	    //WScript.Echo( "\n\n\n str" + c + " is " + str[c] );
-	    //WScript.Echo( "\n urlPart of str" + c + " is " + urlPart );
+    	/* Originally ::
+    	urlPart = urlReg.exec( str[c] ); But Nicole changes into this : */
+    	urlPart = str[c].match( addrReg );
+    	addrArray[c] = urlPart[1].replace(/^[\s　]+/,"" );
+        if ( domainReg.test( addrArray[c] ) ) {
+	        WScript.Echo( "\n" + addrArray[c] + " ==> same domain "); 
+	    /* Oh !! So stupid, Nicole finally found out the exec() does not return a "string", */
+	    /*but it returns an "array"  ! Therefore, the address is not urlPrt, but it's urlPart[1] */
+        }
+        else{
+        	WScript.Echo( "\n" + addrArray[c] + " ==> different domain " ); 
+        }
     }
+    WScript.Echo("-----------------------\n\n");
 }
  
- 
-function test_aTag(){
-    /*
-	two cases for aTag : (1) <a href=\jang>  (2) <a href="http.....">
-	case (2) for aTag is actually urlTag. 
-	*/
+function test_aTag(){    /*	two cases for aTag : (1) <a href=\jang>  (2) <a href="http.....">
+	case (2) for aTag is actually urlTag. */
     aTagReg = /<a\s*href\s*=.*>.*<\/a>/gi;
     str2 = contents.match( aTagReg );
-	WScript.Echo( "\nThere are " + str2.length + "matching results" );
+	WScript.Echo( "\n\nThere are " + str2.length + " matching results\n" );
     for( k=0; k<str2.length; k++ ){
-  
     	urlPart = aTagReg.exec( str2[k] );
-	    //WScript.Echo( "\n\n\n str2" + k + " is " + str2[k] );
-	    //WScript.Echo( "\n urlPart of str2" + k + " is " + urlPart );
-    }
+   }
 }
 
 function test_non_urlTag(){
-    /*
-	two cases for aTag : (1) <a href=\jang>  (2) <a href="http.....">
-	case (2) for aTag is actually urlTag. 
-	*/
-    nUrlReg = /<a\s*href\s*=[^"]*>.*<\/a>/gi;
+	//nUrlReg = /<a\s*href\s*=[^"]*>.*<\/a>/gi;
+	 //urlReg = /<a\s+href\s*=\s*"\s*(http.*?)"\s*>.*?<\/a>.*?/gi
+    nUrlReg = /<a\s*href\s*=\s*([^"]+)>.*?<\/a>.*?/gi;
+    pathReg = /=[\s]*(.*?)[\s]*>/
     str3 = contents.match( nUrlReg );
-	WScript.Echo( "\nThere are " + str3.length + "matching results" );
-    for( j=0; j<str3.length; j++ ){
-  
-    	urlPart = nUrlReg.exec( str3[j] );
-	    WScript.Echo( "\n\n\n str3" + j + " is " + str3[j] );
-	    WScript.Echo( "\n urlPart of str3" + j + " is " + urlPart );
+	WScript.Echo( "\nThere are " + str3.length + " non_url links \n" );
+	nUrlnum = str3.length;
+    for( j=0; j<nUrlnum; j++ ){
+
+    	WScript.Echo( str3[j] );
+
+    	
+    	path = str3[j].match( pathReg );
+    	WScript.Echo( "\npath:" + path );
+    	/**
+    	nUrlArray[j] =  path[1].replace(/^[\s　]+/,"" );
+	    //WScript.Echo( "\n\n\n str3" + j + " is " + str3[j] );
+	    WScript.Echo( "\n urlPart of str3 " + j + " is " + nUrlArray[j] + " ==> same domain" );
+	    /***/
     }
+}
+
+function extract_title( address )
+{ 
+	WScript.Echo( "\nHiiii\n" );//+ address + "---" ); 
+    titleReg = new RegExp( "<title>(.*?)<\/title>", "gi");;
+	
+	for( jj=0; jj<urlNum; jj++ ){
+		address = "http://mirlab.org/jang/courses/webProgramming/homework/linkExtraction/testPage4linkExtraction2.htm";
+		var xmlHttp = new ActiveXObject("Microsoft.XMLHTTP.4.0");
+		WScript.Echo( " 1 ");
+		xmlHttp.open("GET", address, false, "");
+        xmlHttp.send();
+        WScript.Echo( " 1 ");
+        contents2 = xmlHttp.responseText;
+
+        index3 = contents2.search( titleReg );
+        titleArray[jj] = RegExp.$1;
+        WScript.Echo( titleArray[jj] );
+        WScript.Echo( "\nend\n" );
+    }
+/**********************/
+
 }
 
 
 function main(){
+	extract_TA_homework();
     delete_xmpAndComment();
-	test_aTag();
+	//test_aTag();
 	test_urlTag();
 	test_non_urlTag();
-    
+    //extract_title();
 }
 
 main();
@@ -96,3 +131,16 @@ WScript.Echo("index2:" + index2 );
 WScript.Echo("title2:" + RegExp.$2);
 /**/
 
+/* ----Teacher's original code :  extract the title
+----re = new RegExp("<title>(.*?)<\/title>", "gi");
+*/
+/*----  Nicole's Test :  
+----Wrong Code : 
+----urlReg = /<a\s+href\s+=\s+"(.*?)"\s+>.*?<\/a>.*?/
+----Correct Code : 
+----urlReg = /<a\s+href\s*=\s*"(.*?)"\s*>.*?<\/a>.*?/;
+----Correct basic urlReg : 
+----urlReg = /<a\s+href\s*=\s*"\s*(.*?)"\s*>.*?<\/a>.*?/i
+----Or, 
+----re = new RegExp("<a href(.*?)>(.*?)<\/a>(.*?)", "gi");
+*/
